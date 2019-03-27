b8eb02b462151754c08e3a282e57e534bb6d5eac
Meng Li <lm2343635@126.com>
2019-03-27 16:30:41 +0900

    Fix the build warning.

--- Sources/UIGradientExtension.swift
+++ Sources/UIGradientExtension.swift
@@ -10,12 +10,12 @@ import UIKit
 
 public extension UIView {
     
-    public func addGradientWithDirection(_ direction: GradientDirection, colors: [UIColor], cornerRadius: CGFloat = 0, locations: [Double]? = nil) {
+    func addGradientWithDirection(_ direction: GradientDirection, colors: [UIColor], cornerRadius: CGFloat = 0, locations: [Double]? = nil) {
         let gradientLayer = GradientLayer(direction: direction, colors: colors, cornerRadius: cornerRadius, locations: locations)
         self.addGradient(gradientLayer)
     }
     
-    public func addGradient(_ gradientLayer: GradientLayer, cornerRadius: CGFloat = 0) {
+    func addGradient(_ gradientLayer: GradientLayer, cornerRadius: CGFloat = 0) {
         let cloneGradient = gradientLayer.clone()
         cloneGradient.frame = self.bounds
         cloneGradient.cornerRadius = cornerRadius
@@ -25,7 +25,7 @@ public extension UIView {
 
 public extension UIColor {
     
-    public static func hex(_ hex: String, alpha: CGFloat = 1.0) -> UIColor {
+    static func hex(_ hex: String, alpha: CGFloat = 1.0) -> UIColor {
         guard let hex = Int(hex, radix: 16) else { return UIColor.clear }
         return UIColor(red: ((CGFloat)((hex & 0xFF0000) >> 16)) / 255.0,
                        green: ((CGFloat)((hex & 0x00FF00) >> 8)) / 255.0,
@@ -33,12 +33,12 @@ public extension UIColor {
                        alpha: alpha)
     }
     
-    public static func fromGradient(_ gradient: GradientLayer, frame: CGRect, cornerRadius: CGFloat = 0) -> UIColor? {
+    static func fromGradient(_ gradient: GradientLayer, frame: CGRect, cornerRadius: CGFloat = 0) -> UIColor? {
         guard let image = UIImage.fromGradient(gradient, frame: frame, cornerRadius: cornerRadius) else { return nil }
         return UIColor(patternImage: image)
     }
     
-    public static func fromGradientWithDirection(_ direction: GradientDirection, frame: CGRect, colors: [UIColor], cornerRadius: CGFloat = 0, locations: [Double]? = nil) -> UIColor? {
+    static func fromGradientWithDirection(_ direction: GradientDirection, frame: CGRect, colors: [UIColor], cornerRadius: CGFloat = 0, locations: [Double]? = nil) -> UIColor? {
         let gradient = GradientLayer(direction: direction, colors: colors, cornerRadius: cornerRadius, locations: locations)
         return UIColor.fromGradient(gradient, frame: frame)
     }
@@ -46,7 +46,7 @@ public extension UIColor {
 
 public extension UIImage {
     
-    public static func fromGradient(_ gradient: GradientLayer, frame: CGRect, cornerRadius: CGFloat = 0) -> UIImage? {
+    static func fromGradient(_ gradient: GradientLayer, frame: CGRect, cornerRadius: CGFloat = 0) -> UIImage? {
         UIGraphicsBeginImageContextWithOptions(frame.size, false, UIScreen.main.scale)
         guard let ctx = UIGraphicsGetCurrentContext() else { return nil }
         let cloneGradient = gradient.clone()
@@ -58,9 +58,9 @@ public extension UIImage {
         return image
     }
     
-    public static func fromGradientWithDirection(_ direction: GradientDirection, frame: CGRect, colors: [UIColor], cornerRadius: CGFloat = 0, locations: [Double]? = nil) -> UIImage? {
+    static func fromGradientWithDirection(_ direction: GradientDirection, frame: CGRect, colors: [UIColor], cornerRadius: CGFloat = 0, locations: [Double]? = nil) -> UIImage? {
         let gradient = GradientLayer(direction: direction, colors: colors, cornerRadius: cornerRadius, locations: locations)
         return UIImage.fromGradient(gradient, frame: frame)
     }
-}
 
+}
